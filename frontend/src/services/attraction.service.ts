9945e70487d4e39b2cbd925008d8ac7a10bfabfc
Matheus <sodkrumm@gmail.com>
2018-05-03 21:26:12 -0300

    #1 services de atrações e localizações

---
+++ frontend/src/services/attraction.service.ts
@@ -0,0 +1,35 @@
+import {Response} from "express";
+import {Injectable} from "@angular/core";
+import {Observable} from "rxjs/Rx";
+import {Http} from "@angular/http";
+import {environment} from "../environments/environment";
+import "rxjs/add/operator/map";
+import "rxjs/add/observable/of";
+
+export interface Attraction {
+  id: number,
+  name: string,
+  qtyTicketsEntrain: number,
+  timeEntrain: number,
+  location: number,
+  image: number,
+}
+
+@Injectable()
+export class AttractionService {
+
+  constructor(private http: Http) {
+  }
+
+  fixed: Attraction[] = JSON.parse(
+    `[{ "id": 1, "name": "Parque das aguas" }]`);
+
+  public getParks(): Observable<Attraction[]> {
+    return Observable.of(this.fixed);
+    /*
+    return this.http.get(environment.api + 'park?size=100')
+        .map((res: Response) => res.json().content);
+    */
+  }
+
+}
